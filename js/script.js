697d3cd2fe1a5a8457b5baf34d729e4ff42c1a2d
aman-kat <61926048+aman-kat@users.noreply.github.com>
2020-03-08 20:54:23 +0530

    Setting up the website

---
+++ js/script.js
@@ -0,0 +1,46 @@
+$(function(){
+	"use strict";
+	
+	var sect = $( window.location.hash ),
+		portfolio = $('.portfolio-items');
+	
+	if(sect.length == 1){
+		$('.section.active').removeClass('active');
+		sect.addClass('active');
+		if( sect.hasClass('border-d') ){
+			$('body').addClass('border-dark');
+		}
+	}
+	
+	/*=========================================================================
+		Navigation Functions
+	=========================================================================*/
+	$('.section-toggle').on('click', function(){
+		var $this = $(this),
+			sect = $( '#' + $this.data('section') ),
+			current_sect = $('.section.active');
+		if(sect.length == 1){
+			if( sect.hasClass('active') == false && $('body').hasClass('section-switching') == false ){
+				$('body').addClass('section-switching');
+				if( sect.index() < current_sect.index() ){
+					$('body').addClass('up');
+				}else{
+					$('body').addClass('down');
+				}
+				setTimeout(function(){
+					$('body').removeClass('section-switching up down');			
+				}, 2500);
+				setTimeout(function(){
+					current_sect.removeClass('active');
+					sect.addClass('active');
+				}, 1250);
+				if( sect.hasClass('border-d') ){
+					$('body').addClass('border-dark');
+				}else{
+					$('body').removeClass('border-dark');
+				}
+			}
+		}
+	});
+
+});
